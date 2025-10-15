28afdcbf177f4fece1b64ae86164ea460a5aac04
Hussein Ameen <husseinameen99@gmail.com>
2025-10-15 13:34:27 +0300

    edititing on root page

---
+++ lib/features/orderHistory/views/orderHistory_veiw.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OrderhistoryVeiw extends StatelessWidget {
+  const OrderhistoryVeiw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- lib/root.dart
+++ lib/root.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hungr/core/constants/app_colors.dart';
+import 'package:hungr/features/cart/views/cart_view.dart';
+import 'package:hungr/features/chechout/views/checkout_veiw.dart';
+import 'package:hungr/features/home/views/home_view.dart';
+import 'package:hungr/features/orderHistory/views/orderHistory_veiw.dart';
 class Root extends StatefulWidget {
   const Root({super.key});
 
@@ -8,14 +13,38 @@ class Root extends StatefulWidget {
 }
 
 class _RootState extends State<Root> {
+  late PageController controoler;
+  late List<Widget> screens;
+  int currentScreen=0;
+  @override
+
+
+  void initState() {
+    screens=[
+    HomeView(),
+    CartView(),
+    OrderhistoryVeiw(),
+    CheckoutVeiw()
+    ];
+    controoler=PageController(initialPage:currentScreen);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controoler,
         children: [
-          
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.primaryColor,
+          items: [
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+          ]),
     );
   }
 }
