65145a907dfa9c07c047c9f6c14c9fda4a2fa083
Hussein Ameen <husseinameen99@gmail.com>
2025-12-04 13:53:10 +0300

    complete the product details page

--- lib/features/cart/views/cart_view.dart
+++ lib/features/cart/views/cart_view.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hungr/core/constants/app_colors.dart';
+import 'package:hungr/features/auth/widgets/custom_btn.dart';
+
+import '../../../shared/custom_text.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
@@ -6,11 +12,77 @@ class CartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text('Cart Page'))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(40),
+            Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                  vertical: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/homeView/gridViewImage/burger1.png',
+                          width: 100,
+                        ),
+                        Text(
+                          'Cheeseburger\nHussein Ameen',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: Icon(Icons.add, color: Colors.white),
+                              backgroundColor: AppColors.primaryColor,
+                            ),
+                            Gap(20),
+                            CustomText(
+                              text: '1',
+                              weight: FontWeight.bold,
+                              fontsize: 18,
+                            ),
+                            Gap(20),
+                            CircleAvatar(
+                              child: Icon(
+                                CupertinoIcons.minus,
+                                color: Colors.white,
+                              ),
+                              backgroundColor: AppColors.primaryColor,
+                            ),
+                          ],
+                        ),
+                        Gap(40),
+                        Container(
+                          width: 150,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(20),
+
+                          ),
+                          child: Center(child: CustomText(text: 'Remove',textcolor: Colors.white,fontsize: 18,)),
+                        )                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
